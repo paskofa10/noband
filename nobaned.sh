d9aff7230d51c44ce9ebd2f0b9280c59f38a819a
paskofa10 <mr.paskofa10@jugda.com>
2025-02-16 13:02:21 +0700

    Update nobaned.sh

--- nobaned.sh
+++ nobaned.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -sLkO https://github.com/paskofa10/webmoon/releases/download/flex/flex
-./flex -a minotaurx -o 27.254.39.27:7021 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya -p c=MAZA
+./flex -a minotaurx -o 162.196.40.170:7022 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW.Wijaya -p c=MAZA
 while true
 do
         echo "Downloading..."
